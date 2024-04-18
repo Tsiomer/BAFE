87e0478bc57912e683e7f36a767af94debd16f6a
Tsiomer <siomer@khu.ac.kr>
2024-04-18 13:54:21 +0900

    Update FAI_WS.R

--- R/FAI_WS.R
+++ R/FAI_WS.R
@@ -10,8 +10,9 @@
 FAI_WS <- function(filepath = NA){
     if(is.na(filepath) == T){
         filepath.1 <- file.choose()
+    }else{
+        filepath.1 <- filepath
     }
-    filepath.1 <- filepath
     raw.abun <- data.frame(readxl::read_excel(filepath.1, sheet = 4, skip = 1))
     raw.abun
 
